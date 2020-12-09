eb90026aabacdd90c99f50740f7509d380adfcdc
Allan <allanjamil_91@hotmail.com>
2020-12-09 10:55:05 +0100

    Initial commit

---
+++ src/main/java/se/nackademin/stringify/StringifyApplication.java
@@ -0,0 +1,12 @@
+package se.nackademin.stringify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringifyApplication.class, args);
+    }
+}
