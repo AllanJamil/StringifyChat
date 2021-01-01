c1e3dd295701c97a44c3076da5ebb15cbe9d6a96
Allan <allanjamil_91@hotmail.com>
2021-01-01 11:42:00 +0100

    added swagger documentation to MessageController. and changed pathvar to requestparam

--- src/main/java/se/nackademin/stringify/controller/api/MessageController.java
+++ src/main/java/se/nackademin/stringify/controller/api/MessageController.java
@@ -1,7 +1,11 @@
 package se.nackademin.stringify.controller.api;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import se.nackademin.stringify.domain.Message;
 import se.nackademin.stringify.dto.MessageDto;
 import se.nackademin.stringify.service.MessageService;
@@ -17,6 +21,12 @@ public class MessageController {
 
     private final MessageService messageService;
 
+    @ApiOperation(
+            value = "Find the latest messages by chat-id",
+            produces = "application/json",
+            notes = "Returns a collection of the 5 latest messages from a chat session",
+            response = MessageDto.class
+    )
     @GetMapping("latest")
     public List<MessageDto> latestMessageHistory(@RequestParam(name = "chatid") UUID chatGuid) {
         return messageService.getLatestMessages(chatGuid).stream()
@@ -24,8 +34,14 @@ public class MessageController {
                 .collect(Collectors.toList());
     }
 
-    @GetMapping("previous/{page}")
-    public List<MessageDto> providePreviousMessages(@RequestParam(name = "chatid") UUID chatGuid, @PathVariable int page) {
+    @ApiOperation(
+            value = "Find a number of messages by chat-id and page number",
+            produces = "application/json",
+            notes = "Returns a collection of 5 messages in the given page number from a chat session",
+            response = MessageDto.class
+    )
+    @GetMapping("previous")
+    public List<MessageDto> providePreviousMessages(@RequestParam(name = "chatid") UUID chatGuid, @RequestParam int page) {
         return messageService.getPreviousMessage(chatGuid, page).stream()
                 .map(Message::convertToDto)
                 .collect(Collectors.toList());
