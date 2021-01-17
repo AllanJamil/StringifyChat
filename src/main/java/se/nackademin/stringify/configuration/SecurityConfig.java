77851cb6ffa5c3c60a32d725c0da7011a02d79e3
Pedram Shirforoushan <Pedram.Shirforoushan@yh.nackademin.se>
2021-01-17 12:10:02 +0100

    Attempting to disable CORS-origin.

--- src/main/java/se/nackademin/stringify/configuration/SecurityConfig.java
+++ src/main/java/se/nackademin/stringify/configuration/SecurityConfig.java
@@ -1,17 +1,34 @@
 package se.nackademin.stringify.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.applyPermitDefaultValues();
+        corsConfig.addAllowedMethod("DELETE");
+
+        UrlBasedCorsConfigurationSource source = new
+                UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig.applyPermitDefaultValues());
+        return source;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
 
         http.csrf().disable()
                 .authorizeRequests()
